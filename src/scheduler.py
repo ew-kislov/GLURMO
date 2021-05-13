cff0dafdfec3dcd9694af3a8479f2cdde552c3e1
Evgeniy Kislov <evgeniykislov12@gmail.com>
2021-05-13 18:59:55 +0300

    Added basic wiki2 support

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,25 +1,26 @@
-import json
-import datetime
+import os
 
 from dotenv import load_dotenv
+from pprint import pprint
 
 load_dotenv()
 
-from sqlalchemy import or_
-from sqlalchemy.sql.expression import true, false
+# from sqlalchemy import or_
+# from sqlalchemy.sql.expression import true, false
 
-from .config.DbConfig import DbConfig
+# from .config.DbConfig import DbConfig
 
-from .model import User, JobRule, BoundJobRule
+# from .model import User, JobRule, BoundJobRule
 
 from .slurm.SlurmMockService import SlurmMockService
+from .slurm.SlurmWiki2Service import SlurmWiki2Service
 from .slurm import EEvent
 from .slurm import Event
 
 # init db
 
-db_config = DbConfig()
-session = db_config.sesson_factory()
+# db_config = DbConfig()
+# session = db_config.sesson_factory()
 
 # cache
 
@@ -27,83 +28,85 @@ prioritized_jobs = dict()
 
 # scehduler logic functions
 
-def get_hard_priority(uid):
-    try:
-        bound_rules = list(
-            session
-                .query(BoundJobRule)
-                .join(User, isouter=True)
-                .join(JobRule, isouter=True)
-                .filter(JobRule.hard_priority != None)
-                .filter(or_(User.uid == uid, BoundJobRule.is_default == true()))
-        )
-    except:
-        print('Error while processing database query. Please tru again later.')
+# def get_hard_priority(uid):
+#     try:
+#         bound_rules = list(
+#             session
+#                 .query(BoundJobRule)
+#                 .join(User, isouter=True)
+#                 .join(JobRule, isouter=True)
+#                 .filter(JobRule.hard_priority != None)
+#                 .filter(or_(User.uid == uid, BoundJobRule.is_default == true()))
+#         )
+#     except:
+#         print('Error while processing database query. Please tru again later.')
 
-    uid_bound_rules = list(filter(lambda rule: rule.user and rule.user.uid == uid, bound_rules))
+#     uid_bound_rules = list(filter(lambda rule: rule.user and rule.user.uid == uid, bound_rules))
 
-    if uid_bound_rules and uid_bound_rules[0] != None:
-        return uid_bound_rules[0].job_rule.hard_priority
+#     if uid_bound_rules and uid_bound_rules[0] != None:
+#         return uid_bound_rules[0].job_rule.hard_priority
 
-    default_bound_rules = list(filter(lambda rule: rule.is_default == True, bound_rules))
+#     default_bound_rules = list(filter(lambda rule: rule.is_default == True, bound_rules))
 
-    if default_bound_rules and default_bound_rules[0] != None:
-        return default_bound_rules[0].job_rule.hard_priority
+#     if default_bound_rules and default_bound_rules[0] != None:
+#         return default_bound_rules[0].job_rule.hard_priority
 
-    raise Exception('No priorities found for this job.')
-
-
-def queue_job(job):
-    hard_priority = get_hard_priority(job['user'])
-
-    if hard_priority not in prioritized_jobs:
-        prioritized_jobs[hard_priority] = [ job ]
-    else:
-        prioritized_jobs[hard_priority].append(job)
+#     raise Exception('No priorities found for this job.')
 
 
-def start_next_job():
-    if not prioritized_jobs.keys():
-        print("No jobs in queue")
-        return
+# def queue_job(job):
+#     hard_priority = get_hard_priority(job['user'])
 
-    # TODO: check for required nodes, preferences etc
+#     if hard_priority not in prioritized_jobs:
+#         prioritized_jobs[hard_priority] = [ job ]
+#     else:
+#         prioritized_jobs[hard_priority].append(job)
 
-    first_hard_priority = min(prioritized_jobs.keys())
-    first_priority_job = prioritized_jobs[first_hard_priority].pop(0)
 
-    if not prioritized_jobs[first_hard_priority]:
-        del prioritized_jobs[first_hard_priority]
+# def start_next_job():
+#     if not prioritized_jobs.keys():
+#         print("No jobs in queue")
+#         return
 
-    slurm_service.start_job(first_priority_job)
+#     # TODO: check for required nodes, preferences etc
+
+#     first_hard_priority = min(prioritized_jobs.keys())
+#     first_priority_job = prioritized_jobs[first_hard_priority].pop(0)
+
+#     if not prioritized_jobs[first_hard_priority]:
+#         del prioritized_jobs[first_hard_priority]
+
+#     slurm_service.start_job(first_priority_job)
 
 # define slurm job event handler
 
 def process_slurm_event(event):
-    if (event.get_type() == EEvent.JobQueued):
-        print(f"New job arrived")
-        queue_job(event.get_payload())
-        start_next_job()
-    elif (event.get_type() == EEvent.JobDone):
-        print(f"Job with id #{event.get_payload()} done")
-        start_next_job()
-    elif (event.get_type() == EEvent.JobDeclined):
-        print(f"Job with id #{event.get_payload()} declined")
-    elif (event.get_type() == EEvent.JobStarted):
-        print(f"Job with id #{event.get_payload()} started")
+    # if (event.get_type() == EEvent.JobQueued):
+    #     print(f"New job arrived")
+    #     queue_job(event.get_payload())
+    #     start_next_job()
+    # elif (event.get_type() == EEvent.JobDone):
+    #     print(f"Job with id #{event.get_payload()} done")
+    #     start_next_job()
+    # elif (event.get_type() == EEvent.JobDeclined):
+    #     print(f"Job with id #{event.get_payload()} declined")
+    # elif (event.get_type() == EEvent.JobStarted):
+    #     print(f"Job with id #{event.get_payload()} started")
+
+    if (event.get_type() == EEvent.JobDone):
+        pprint(event.get_payload())
 
 
 # init slurm service, set event handler, emit queued event on slurm waiting jobs
 
-slurm_service = SlurmMockService('127.0.0.1', 8080)
+# slurm_service = SlurmMockService('127.0.0.1', 8080)
+
+slurm_service = SlurmWiki2Service()
 
 slurm_service.set_subscriber(process_slurm_event)
 
 for job in slurm_service.get_jobs():
     process_slurm_event(Event(EEvent.JobQueued, job))
 
-# force to run mock job
 
-first_job = {'id': 14, 'user': 10, 'group': 1, 'nodes_required': 5}
-
-slurm_service.start_job(first_job)
+slurm_service.run()
--- src/slurm/SlurmWiki2Service.py
+++ src/slurm/SlurmWiki2Service.py
@@ -1,16 +1,78 @@
-import yaml
 import socket
-import datetime
+import os;
 
-from slurm.SlurmService import SlurmService
+from .SlurmService import SlurmService
+
+from .EEvent import EEvent
+from .Event import Event
 
 class SlurmWiki2Service(SlurmService):
     def __init__(self, config):
         self.__init_connection(config)
 
-    def __init_connection(self, config):
-        host = config.get('host')
-        port = config.get('wiki2_port')
+        self.__subscribers = []
 
-        self.__socket = socket.socket()
-        self.__socket.connect((host, port))
+        self.__host = os.getenv("WIKI2_HOST")
+        self.__wiki2_port = os.getenv("WIKI2_PORT")
+        self.__port_for_wiki2 = os.getenv("PORT_FOR_WIKI2")
+
+    def __init_connection(self):
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket.bind((self.__host, self.__port_for_wiki2))
+        self.__socket.listen(10)
+
+    def set_subscriber(self, subscriber):
+        self.__subscribers.append(subscriber)
+
+    def run(self):
+        while True:
+            conn, addr = self.__socket.accept()
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(4)
+                if not data:
+                    break
+
+                if data.strip() == '1234':
+                    jobs = self.__get_jobs(conn)
+
+                    for subscriber in self.__subscribers:
+                        subscriber(Event(EEvent.JobDone, jobs[-1]))
+
+    def __get_jobs(self, conn):
+        client_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_fd.connect((self.__host, self.__wiki2_port))
+        
+        command = 'AUTH=slurm DT=SC=-300 TS=1620048581 CMD=GETJOBS ARG=0:ALL'
+        header = str(len(command)).zfill(8)
+        conn.send(str.encode(header))
+        conn.send(str.encode('AUTH=slurm DT=SC=-300 TS=1620048581 CMD=GETJOBS ARG=0:ALL'))
+
+        header = int(client_fd.recv(8).decode())
+        jobsRaw = client_fd.recv(header).decode()
+
+        jobs = self.__parse_jobs(jobsRaw)
+
+        client_fd.close()
+
+        return jobs
+
+    def __parse_jobs(self, jobsRaw):
+        jobsRawList = jobsRaw.split('#')
+
+        jobsRawList.pop(0)
+
+        jobs = []
+
+        for jobRaw in jobsRawList:
+            pairs = jobRaw[(jobRaw.find(':') + 1):].split(';')
+            job = dict()
+            job['id'] = jobRaw[0:(jobRaw.find(':'))]
+            for pair in pairs:
+                pairArr = pair.split('=')
+                if len(pairArr) != 2:
+                    continue
+                job[pairArr[0]] = pairArr[1]
+            jobs.append(job)
+
+        return jobs
